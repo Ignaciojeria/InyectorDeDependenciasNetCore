ac88e757cb2cced2402dcfd88ba7f1c503540ede
ignacio <Ignaciovl.j@gmail.com>
2017-11-04 19:26:21 -0300

    Documentando a mi manera las caracteristicas de inyector de dependencias de netCore

--- Program.cs
+++ Program.cs
@@ -15,20 +15,29 @@ namespace NetCoreDI
              1.-Contiene las instancias que vamos a inyectar a nuestras clases
              2.- las instancias a inyectar se pueden definir como Singleton,Scopes o Transient
              
-            a.-Singleton nos provee una única instancia del contenedor que será
-                provista siempre que le pidamos al inyector de dependencias ese servicio.
-
-             b.-Deberás estudiar el comportamiento de un Scope! xD
+            //Instancia global y única. Consume pocos recursos pero no nos serviría trabajar en las request de un api
+            // donde implique hacer persistencia de datos.
+            a.-Singleton: nos provee una única instancia del contenedor que será
+                provista siempre que le pidamos al inyector de dependencias el uso de ese servicio.
             
-            c.-Un transient devuelve siempre una nueva instancia del servicio
-               es cómo cuando trabajamos con dbContext y lo utilizamos per request.
+
+            //un poco más eficiente y nos serviría para inyectar dependenciar en un api rest por ejemplo.
+             b.-Scope:el contenedor nos provee un singleton pero cada vez que
+                      hacemos una solicitud de ese singleton se crea uno nuevo a partir de esa instancia a nivel de request,
+                      es decir, que llamas a un singleton, al hacer una solicitud del uso de ese servicio que contiene
+                      un singleton crea internamente otra instancia única a partir del singleton que contiene el contenedor
+                      y puedes modificar esa instancia de forma local a lo largo del request sin tocar el singleton global
+                      es cómo una nueva instancia per request (por solicitud) a nivel de scope.
+                      
+            //consume más recursos.
+            c.-Un transient devuelve siempre una nueva instancia del servicio cada vez que lo solicitemos.
              */
-            var service = new ServiceCollection();
-            service.AddSingleton<ISaludo, Saludo>();
-            service.AddSingleton<IHogar, Hogar>();
+            var services = new ServiceCollection();
+            services.AddSingleton<ISaludo, Saludo>();
+            services.AddSingleton<IHogar, Hogar>();
 
             //la instancia saludo 1 y saludo 2 son la misma instancia que nos provee el serviceProvider
-            var serviceProvider = service.BuildServiceProvider();
+            var serviceProvider = services.BuildServiceProvider();
 
             ISaludo saludo1 = serviceProvider.GetService<ISaludo>();
 
@@ -43,6 +52,13 @@ namespace NetCoreDI
             saludo2.Saludar();
             Console.WriteLine(saludo1.GetHashCode().ToString());
             Console.WriteLine(saludo2.GetHashCode().ToString());
+
+            /*Ejemplo Scope Que toma un singleton y en base a ese singleton crea una nueva instancia dentro de la petición
+             (Instancia per request en este ejemplo puntual) */
+            using (var scope = services.BuildServiceProvider().CreateScope()) {
+                var saludo3 = scope.ServiceProvider.GetService<ISaludo>();
+                Console.WriteLine(saludo3.GetHashCode().ToString());
+            }
 
         }
     }
